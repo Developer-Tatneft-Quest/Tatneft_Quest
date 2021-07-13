92c7109df56182e3e83d9ade00ffbc726d1b15ad
Mioler1 <graveer3759@mail.ru>
2021-07-13 10:46:01 +0300

    added image for Tatneft Quest

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home page'),
+      home: MyHomePage(title: 'Flutter demo home page'),
     );
   }
 }
